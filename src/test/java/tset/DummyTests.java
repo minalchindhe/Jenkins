ebe4687ee463133a7855e6c07aa5d333b895f1a8
minalchindhe <91877182+minalchindhe@users.noreply.github.com>
2022-02-07 17:46:17 +0530

    Update DummyTests.java

--- src/test/java/tset/DummyTests.java
+++ src/test/java/tset/DummyTests.java
@@ -6,29 +6,4 @@ public class DummyTests {
   protected void run() {
     System.out.print("Hello");
   }
-
-  @Test public void test1() {
-    run();
-  }
-
-  @Test public void test2() {
-    run();
-  }
-
-  @Test public void test3() {
-    run();
-  }
-
-  @Test public void test4() {
-    run();
-  }
-
-  @Test public void test5() {
-    run();
-  }
-
-  @Test public void test6() {
-    run();
-  }
-
 }
